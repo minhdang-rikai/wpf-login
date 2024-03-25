ec760063f453835b6bdcdc10072a27522e265a41
Dang Minh <dtminh2808@gmail.com>
2024-03-25 09:26:44 +0700

    update

--- AutoLogin/Program.cs
+++ AutoLogin/Program.cs
@@ -6,6 +6,9 @@ using System.IO;
 using System.Threading;
 using System.Windows.Automation;
 using System.Windows.Media.Imaging;
+using System.Windows.Forms;
+using System.Windows.Media;
+using System.Windows;
 
 namespace AutoLoginApp
 {
@@ -27,7 +30,7 @@ namespace AutoLoginApp
             }
 
             // chụp lại màn hình khi app mở
-            SaveToExcel(CaptureScreen(), "LoginScreen_StartApp");
+            SaveToExcel(CaptureWindow(loginWindow), "LoginScreen_StartApp");
 
             // phần tự động nhập username, password và click btn login
             AutomationElement usernameTextBox = loginWindow.FindFirst(TreeScope.Descendants, new PropertyCondition(AutomationElement.AutomationIdProperty, "UsernameTextBox"));
@@ -43,24 +46,76 @@ namespace AutoLoginApp
             InvokePattern invokePattern = (InvokePattern)loginButton.GetCurrentPattern(InvokePattern.Pattern);
             invokePattern.Invoke();
 
+            Thread.Sleep(1000);
             // chụp lại màn khi sau khi bấm nút login
-            SaveToExcel(CaptureScreen(), "LoginScreen_AfterClickLogin");
+            SaveToExcel(CaptureWindow(loginWindow), "LoginScreen_AfterClickLogin");
         }
 
-        static Bitmap CaptureScreen()
+        // chụp ảnh full screen
+        //static Bitmap CaptureScreen()
+        //{
+        //    Bitmap bmpScreen = new Bitmap(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width,
+        //                                   System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
+        //    using (Graphics g = Graphics.FromImage(bmpScreen))
+        //    {
+        //      g.CopyFromScreen(System.Windows.Forms.Screen.PrimaryScreen.Bounds.X,
+        //                       System.Windows.Forms.Screen.PrimaryScreen.Bounds.Y,
+        //                       0, 0,
+        //                       bmpScreen.Size,
+        //                       CopyPixelOperation.SourceCopy);
+        //    }
+
+        //    return bmpScreen;
+        //}
+
+        static Bitmap CaptureWindow(AutomationElement windowElement)
         {
-            Bitmap bmpScreen = new Bitmap(System.Windows.Forms.Screen.PrimaryScreen.Bounds.Width,
-                                           System.Windows.Forms.Screen.PrimaryScreen.Bounds.Height);
-            using (Graphics g = Graphics.FromImage(bmpScreen))
+            Rect windowRect = windowElement.Current.BoundingRectangle;
+
+            int width = (int)windowRect.Width;
+            int height = (int)windowRect.Height;
+
+            Bitmap bmp = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+
+            using (Graphics graphics = Graphics.FromImage(bmp))
             {
-                g.CopyFromScreen(System.Windows.Forms.Screen.PrimaryScreen.Bounds.X,
-                                 System.Windows.Forms.Screen.PrimaryScreen.Bounds.Y,
-                                 0, 0,
-                                 bmpScreen.Size,
-                                 CopyPixelOperation.SourceCopy);
+                graphics.CopyFromScreen((int)windowRect.Left, (int)windowRect.Top, 0, 0, new System.Drawing.Size(width, height), CopyPixelOperation.SourceCopy);
             }
 
-            return bmpScreen;
+            CaptureChildren(windowElement, bmp);
+
+            return bmp;
+        }
+
+        static void CaptureChildren(AutomationElement parentElement, Bitmap bmp)
+        {
+            AutomationElementCollection children = parentElement.FindAll(TreeScope.Children, System.Windows.Automation.Condition.TrueCondition);
+            foreach (AutomationElement child in children)
+            {
+                Rect rect = child.Current.BoundingRectangle;
+
+                if (!rect.IsEmpty && rect.Width > 0 && rect.Height > 0)
+                {
+                    using (Graphics graphics = Graphics.FromImage(bmp))
+                    {
+                        int x = (int)rect.Left - (int)parentElement.Current.BoundingRectangle.Left;
+                        int y = (int)rect.Top - (int)parentElement.Current.BoundingRectangle.Top;
+
+                        Bitmap childBmp = new Bitmap((int)rect.Width, (int)rect.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                        using (Graphics childGraphics = Graphics.FromImage(childBmp))
+                        {
+                            childGraphics.CopyFromScreen((int)rect.Left, (int)rect.Top, 0, 0, new System.Drawing.Size((int)rect.Width, (int)rect.Height), CopyPixelOperation.SourceCopy);
+                        }
+
+                        graphics.DrawImage(childBmp, x, y);
+                    }
+                }
+
+                if (TreeWalker.ControlViewWalker.GetFirstChild(child) != null)
+                {
+                    CaptureChildren(child, bmp);
+                }
+            }
         }
 
         static void SaveToExcel(Bitmap screenshot, string screeenName)
